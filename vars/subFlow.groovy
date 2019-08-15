0c2680200c9cf96216f1b2fa653f5ff04b005047
ercanales <enrique.r.canales@gmail.com>
2019-08-15 12:29:46 -0500

    Update subFlow.groovy

--- vars/subFlow.groovy
+++ vars/subFlow.groovy
@@ -2,7 +2,7 @@ def call(Map config) {
   
     TestAuth(
         token: "${config.SA_TOKEN}",
-      project: config.PROJECT_CONFIG.getAt('GCP_PROJECT', 5)
+      project: config.PROJECT_CONFIG.getAt(GCP_PROJECT, 5)
         //project: "x"
     )
 
