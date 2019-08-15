1e7629af09cd80d31d5ca679f675d8f477892b54
ercanales <enrique.r.canales@gmail.com>
2019-08-15 11:33:44 -0500

    Create devProjectConfig.groovy

---
+++ vars/devProjectConfig.groovy
@@ -0,0 +1,36 @@
+def call(body) {
+  def pipelineParams = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = pipelineParams
+  body()
+
+  def jenkinsAgent = pipelineParams?.agent ? "${pipelineParams.agent}" : "jenkins-agent"
+
+  pipeline {
+    agent none
+
+    environment {
+      APPLICATION_NAME = "${pipelineParams.app}"
+    }
+
+    stages {
+
+      stage("Update Dev Objects") {
+        agent {label "${jenkinsAgent}"}
+        
+        environment{
+          PROJECT_SA_TOKEN = credentials("${pipelineParams.devProjectConfig.PROJECT}-jenkins-sa")
+        }
+
+        steps {
+
+          subFlow(
+            PROJECT_CONFIG: "${pipelineParams.devProjectConfig}",
+            SA_TOKEN: env.PROJECT_SA_TOKEN
+          )
+
+        } // steps
+      } // stage
+    }
+  }
+}
